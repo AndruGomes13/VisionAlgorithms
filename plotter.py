a6949764446b7b22a9bf9179b9c64f38a12cfbe2
Jose Lavariega <josea.lavariegago@gmail.com>
2023-11-25 17:34:47 +0100

    basic pointcloud plotting

--- plotter.py
+++ plotter.py
@@ -1,18 +1,55 @@
 # main imports
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+import time
+
+
 from matplotlib.lines import Line2D
 # internal imports
 from visual_odometry import Visual_Odometry
 
 
 class Plotter:
+    def __init__(self) -> None:
+        self.estimate_size = 6 # size of the state estimate: x,y,z,theta,phi,psi 
+        self.first_pose    = np.array([0,0,0,1]) # in generalized coordinates
+        self.estimated_pose_history = np.zeros((3,20)) # can grow to be more. 
 
-    def plot_pointcloud_3d(pointcloud):
-        print("I am printing a pointcloud!")
+        self.last_50_poses = np.empty((self.estimate_size, 50))
+        self.lidar_cmap = plt.get_cmap('hsv')
 
 
-    def plot_path(poses):
+    def plot_pointcloud_3d(self, pointCloud): # TODO: pointcloud Object assert, Current placeholders for pointcloud object
+        x = pointCloud[:,0]
+        y = pointCloud[:,1]
+        z = pointCloud[:,2]
+        dist = pointCloud[:,3]
+
+        fig_pointCloud = plt.figure()
+        ax  = fig_pointCloud.add_subplot(projection='3d')
+        img = ax.scatter(x, y, z, c= dist, cmap=self.lidar_cmap)
+        fig_pointCloud.colorbar(img)
+
+        plt.show()
+
+
+    def plot_pointcloud_topdown(self, pointCloud):
+        x = pointCloud[:,0]
+        y = pointCloud[:,1]
+        z = pointCloud[:,2]
+        dist = pointCloud[:,3]
+
+        fig_pointCloud = plt.figure()
+        ax  = fig_pointCloud.add_subplot()
+        img = ax.scatter(x, y, c= dist, cmap=self.lidar_cmap)
+        fig_pointCloud.colorbar(img)
+
+        plt.show()
+
+
+    def plot_path(self, poses):
         print("These are the poses!")
+
 
