be063beb6c87dfaad02f294b527a8e59684c7336
Jose Lavariega <josea.lavariegago@gmail.com>
2023-11-25 13:12:49 +0100

    plotting class skeleton

---
+++ plotter.py
@@ -0,0 +1,18 @@
+# main imports
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+# internal imports
+from visual_odometry import Visual_Odometry
+
+
+class Plotter:
+
+    def plot_pointcloud_3d(pointcloud):
+        print("I am printing a pointcloud!")
+
+
+    def plot_path(poses):
+        print("These are the poses!")
+
